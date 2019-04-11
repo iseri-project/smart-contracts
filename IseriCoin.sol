8b304a5ae849d8aea375a9b7583082e5f591fd69
iseri-team <48780256+iseri-team@users.noreply.github.com>
2019-04-11 14:48:37 +0300

    Update IseriCoin.sol

--- IseriCoin.sol
+++ IseriCoin.sol
@@ -1,131 +1,108 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.4.23;
 
-library SafeMath {
-    function add(uint a, uint b) internal pure returns (uint c) {
-        c = a + b;
-        require(c >= a);
-    }
-    function sub(uint a, uint b) internal pure returns (uint c) {
-        require(b <= a);
-        c = a - b;
-    }
-    function mul(uint a, uint b) internal pure returns (uint c) {
-        c = a * b;
-        require(a == 0 || c / a == b);
-    }
-    function div(uint a, uint b) internal pure returns (uint c) {
-        require(b > 0);
-        c = a / b;
-    }
-}
+
+// ----------------------------------------------------------------------------
+// IseriCoin TRC20 Token
+//
+// Main wallet : TPQLPbB8iabMbvcy6x3DfxjCRmYfca7Qav
+// Symbol         : IRC
+// Name           : IseriCoin
+// Total supply   : 100 000 000 000
+// Decimals       : 18
+//
+// (c) by Iseri Inc. 2019
+// ----------------------------------------------------------------------------
 
 contract ERC20Interface {
     function totalSupply() public view returns (uint);
     function balanceOf(address tokenOwner) public view returns (uint balance);
-    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
     function transfer(address to, uint tokens) public returns (bool success);
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
     function approve(address spender, uint tokens) public returns (bool success);
     function transferFrom(address from, address to, uint tokens) public returns (bool success);
-
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
-
-contract ApproveAndCallFallBack {
-    function receiveApproval(address from, uint256 tokens, address token, bytes memory data) public;
-}
-
-contract Owned {
-    address public owner;
-    address public newOwner;
-
-    event OwnershipTransferred(address indexed _from, address indexed _to);
-
-    constructor() public {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address _newOwner) public onlyOwner {
-        newOwner = _newOwner;
-    }
-    function acceptOwnership() public {
-        require(msg.sender == newOwner);
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-        newOwner = address(0);
-    }
-}
-
-contract FixedSupplyToken is ERC20Interface, Owned {
-    using SafeMath for uint;
-
-    string public symbol;
-    string public  name;
-    uint8 public decimals;
-    uint _totalSupply;
-
-    mapping(address => uint) balances;
+    // ------------------------------------------------------------------------
+    // TRC20 Token, with the addition of symbol, name and decimals supply and founder
+    // ------------------------------------------------------------------------
+contract IseriCoin is ERC20Interface{
+    string public name = "IseriCoin";
+    string public symbol = "IRC";
+    uint8 public decimals = 18;
+    // (100 b with 18 decimals) 
+    uint public supply; 
+    address public founder;
+    mapping(address => uint) public balances;
     mapping(address => mapping(address => uint)) allowed;
-
-    constructor() public {
-        symbol = "IRC";
-        name = "Iseri Coin";
-        decimals = 18;
-        _totalSupply = 100000000000 * 10**uint(decimals);
-        balances[owner] = _totalSupply;
-        emit Transfer(address(0), owner, _totalSupply);
+    //allowed[0x111...owner][0x2222...spender] = 100;
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    // ------------------------------------------------------------------------
+    // Constructor With 100 000 000 000 supply, All deployed tokens sent to Main wallet
+    // ------------------------------------------------------------------------
+    constructor() public{
+        supply = 300000000000000000000000000000;
+        founder = msg.sender;
+        balances[founder] = supply;
     }
-
-    function totalSupply() public view returns (uint) {
-        return _totalSupply.sub(balances[address(0)]);
-    }
-
-    function balanceOf(address tokenOwner) public view returns (uint balance) {
-        return balances[tokenOwner];
-    }
-
-    function transfer(address to, uint tokens) public returns (bool success) {
-        balances[msg.sender] = balances[msg.sender].sub(tokens);
-        balances[to] = balances[to].add(tokens);
-        emit Transfer(msg.sender, to, tokens);
-        return true;
-    }
-
-    function approve(address spender, uint tokens) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        return true;
-    }
-
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        balances[from] = balances[from].sub(tokens);
-        allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
-        balances[to] = balances[to].add(tokens);
-        emit Transfer(from, to, tokens);
-        return true;
-    }
-
-    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+    // ------------------------------------------------------------------------
+    // Returns the amount of tokens approved by the owner that can be
+    // transferred to the spender's account
+    // ------------------------------------------------------------------------
+    function allowance(address tokenOwner, address spender) public view returns(uint){
         return allowed[tokenOwner][spender];
     }
-
-    function approveAndCall(address spender, uint tokens, bytes memory data) public returns (bool success) {
+    // ------------------------------------------------------------------------
+    // Token owner can approve for spender to transferFrom(...) tokens
+    // from the token owner's account
+    // ------------------------------------------------------------------------
+    function approve(address spender, uint tokens) public returns(bool){
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
-        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, address(this), data);
         return true;
     }
+    // ------------------------------------------------------------------------
+    //  Transfer tokens from the 'from' account to the 'to' account
+    // ------------------------------------------------------------------------
+    function transferFrom(address from, address to, uint tokens) public returns(bool){
+        require(allowed[from][msg.sender] >= tokens);
+        require(balances[from] >= tokens);
 
-    function () external payable {
-        revert();
+        balances[from] -= tokens;
+        balances[to] += tokens;
+        allowed[from][msg.sender] -= tokens;
+
+        emit Transfer(from, to, tokens);
+
+        return true;
     }
-
-    function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
-        return ERC20Interface(tokenAddress).transfer(owner, tokens);
+    // ------------------------------------------------------------------------
+    // Public function to return supply
+    // ------------------------------------------------------------------------
+    function totalSupply() public view returns (uint){
+        return supply;
+    }
+    // ------------------------------------------------------------------------
+    // Public function to return balance of tokenOwner
+    // ------------------------------------------------------------------------
+    function balanceOf(address tokenOwner) public view returns (uint balance){
+        return balances[tokenOwner];
+    }
+    // ------------------------------------------------------------------------
+    // Public Function to transfer tokens
+    // ------------------------------------------------------------------------
+    function transfer(address to, uint tokens) public returns (bool success){
+        require(balances[msg.sender] >= tokens && tokens > 0);
+        balances[to] += tokens;
+        balances[msg.sender] -= tokens;
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    } 
+    // ------------------------------------------------------------------------
+    // Revert function to NOT accept TRX
+    // ------------------------------------------------------------------------
+    function () public payable {
+        revert();
     }
 }
